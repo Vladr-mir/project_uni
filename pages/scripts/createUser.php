eac95c33056ee5e2fb3bd496dbca4d060b29e14e
Vladimir <duenasvladimir@gmail.com>
2024-05-26 23:01:23 -0600

    Fix redirection page

--- pages/scripts/createUser.php
+++ pages/scripts/createUser.php
@@ -8,19 +8,21 @@ $direccion = $_POST['direccion'];
 $password = $_POST['password'];
 $telefono = $_POST['telefono'];
 
-$connection = connectDB();
-$query = "INSERT INTO `clientes` (`nombres`, `apellidos`, `direccion`, `telefono`);
-VALUES ('$nombre', '$apellido', '$direccion', '$password');";
+$connection = $connectDB();
+$query = "INSERT INTO `clientes` (`nombres`, `apellidos`, `direccion`, `telefono`)
+VALUES ('$nombre', '$apellido', '$direccion', '$telefono');";
 $result = mysqli_query($connection, $query);
 
-$query = "INSERT INTO `usuarios` (`email`, `password`);
-VALUES ('$email', '$password');";
+$last_id = mysqli_insert_id($connection);
+
+$query = "INSERT INTO `usuarios` (`email`, `password`, `idCliente`)
+VALUES ('$email', '$password', '$last_id');";
 $result = mysqli_query($connection, $query);
 
 mysqli_close($connection);
 
 if ($result) {
-  header('Location: ../forms/proveedores.php');
+  header('Location: ../forms/login.html');
 } else {
   header('Location: ../error.html');
 }
